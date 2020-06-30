68ca6c9e6b45f1fbd5ad1a3cb3408a8e999d583f
Živilė Valiuškaitė <44140767+zivval2@users.noreply.github.com>
2020-06-30 08:50:57 +0300

    Migrations

---
+++ bakis/Migrations/20200629100937_Competitor table.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bakis.Migrations
+{
+    public partial class Competitortable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Competitor",
+                columns: table => new
+                {
+                    CompetitorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Adress = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Competitor", x => x.CompetitorId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Competitor");
+        }
+    }
+}
